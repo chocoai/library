431d1dcebb054e9fdb97624fade16be771fcca16
kavonm <mjsc1023@gmail.com>
2018-05-24 16:29:02 +0800

    quote

---
+++ go/quote.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+//reference: https://blog.csdn.net/wanchope/article/details/53413760
+func main() {
+    fmt.Println("How are you?")
+    fmt.Println("Fine, thank you. And you?")
+    fmt.Println("I'm fine too.")
+
+    fmt.Println(`
+        How are you?
+        Fine, thank you. And you?
+        I'm fine too.
+    `)
+}
