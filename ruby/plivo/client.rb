61b796454ef141c8d3a5a91426d4e58e166b675b
kavonm <mjsc1023@gmail.com>
2018-09-13 18:28:35 +0800

    update

--- ruby/plivo/client.rb
+++ ruby/plivo/client.rb
@@ -6,7 +6,7 @@ include Plivo
 client = RestClient.new('MAMZI3MJZLNGFKMTK1NZ', 'MDZkOTRkMjY2NDY1Mjc2MmQwODNlMDhhOWZkZjE3')
 
 client.messages.create(
-  '1111111111',
-  %w[+8618661827982],
+  '+14848747788',
+  %w[8618661827982],
   'Hello, Majun!'
 )
